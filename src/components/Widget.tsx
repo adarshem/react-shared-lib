f0faf2bc6303dcc8342d989af77872355f33b2d7
adarshem <adarshem@gmail.com>
2025-06-21 16:38:37 -0700

    feat: udpated widget to use description prop

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 interface WidgetProps {
   title: string;
+  description: string;
 }
 
-function Widget({ title }: WidgetProps) {
-  return <div>{title}</div>;
+function Widget({ title , description}: WidgetProps) {
+  return (
+    <>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </>
+  )
 }
 
 export default Widget;
