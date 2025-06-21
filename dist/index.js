1d6b2751b7bcf89bf3ab5aadd597b0c2c3b3d01c
adarshem <adarshem@gmail.com>
2025-06-21 15:25:13 -0700

    chore: set up semantic-release

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Widget", {
+  enumerable: true,
+  get: function get() {
+    return _Widget["default"];
+  }
+});
+var _Widget = _interopRequireDefault(require("./components/Widget"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
---
+++ src/components/Widget.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Widget = () => {
+  return <div>Hello from Widget!</div>;
+};
+
+export default Widget;
